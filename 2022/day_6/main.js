2ceb3426684aecc3ed05386d474c3e441f86f191
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-01 03:49:43 +0100

    Add part 2 solution for day 6 task

--- 2022/day_6/main.js
+++ 2022/day_6/main.js
@@ -1,20 +1,24 @@
 const fs = require("fs");
 
-function findSoPMarker(datastream) {
-    for(let index = 0; index < datastream.length - 3; ++index) {
-        const sequence = new Set([...datastream.slice(index, index + 4)]);
+function findMarkerStart(datastream, markerLength) {
+    for(let index = 0; index < datastream.length - (markerLength - 1); ++index) {
+        const sequence = new Set([...datastream.slice(index, index + markerLength)]);
         
-        if(sequence.size === 4) return index + 4;
+        if(sequence.size === markerLength) return index;
     }
-    return "Start of packet marker not found.";
+    return "Marker not found.";
 }
 
 try {
     const input = fs.readFileSync("./input.txt", "utf-8").replace(/\r/g, "").split("\n\n")[0];
+    const sopMarkerLength = 4;
+    const somMarkerLength = 14;
 
-    const characterCount = findSoPMarker(input);
+    const charCountBeforeSoP = findMarkerStart(input, sopMarkerLength) + sopMarkerLength;
+    const charCountBeforeSoM = findMarkerStart(input, somMarkerLength) + somMarkerLength;
 
-    console.log(characterCount);
+    console.log(`Character count before the first start-of-packet marker is ${charCountBeforeSoP}.`);
+    console.log(`Character count before the first start-of-message marker is ${charCountBeforeSoM}.`);
 } catch (error) {
     console.error(error);
 }
