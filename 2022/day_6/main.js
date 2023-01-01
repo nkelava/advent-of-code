259e88b5b3822e98d50f4f4746f040f3756d79c2
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-01 03:29:18 +0100

    Add part one solution for day 6 task

---
+++ 2022/day_6/main.js
@@ -0,0 +1,21 @@
+const { setDefaultResultOrder } = require("dns");
+const fs = require("fs");
+
+function findSoPMarker(datastream) {
+    for(let index = 0; index < datastream.length - 3; ++index) {
+        const sequence = new Set([...datastream.slice(index, index + 4)]);
+        
+        if(sequence.size === 4) return index + 4;
+    }
+    return "Start of packet marker not found.";
+}
+
+try {
+    const input = fs.readFileSync("./input.txt", "utf-8").replace(/\r/g, "").split("\n\n")[0];
+
+    const characterCount = findSoPMarker(input);
+
+    console.log(characterCount);
+} catch (error) {
+    console.error(error);
+}
