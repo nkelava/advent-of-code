e7ac655aff7625170c3626824f535c648097f03c
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-03 03:37:19 +0100

    Rename variables in part 2 solution for day 7 task

--- 2022/day_7/main.js
+++ 2022/day_7/main.js
@@ -6,11 +6,11 @@ try {
     const sizeLimit = 100001;
     const fileSystemTree = customFileSystem.createFileSystemTree(input);
 
-    const sizeToRemove = 30000000 - (70000000 - fileSystemTree.root.size); 
-    const directoryToRemove = customFileSystem.findDirectoryToDelete(fileSystemTree, sizeToRemove);
+    const sizeToDelete = 30000000 - (70000000 - fileSystemTree.root.size); 
+    const directoryToDelete = customFileSystem.findDirectoryToDelete(fileSystemTree, sizeToDelete);
 
     console.log(`Sum of all directories with size lower than ${sizeLimit - 1} is ${fileSystemTree.sumSizeLowerThan(sizeLimit)}.`);
-    console.log(`Closest total size of the directory that has to be deleted is ${directoryToRemove.size}.`);
+    console.log(`Closest total size of the directory that has to be deleted is ${directoryToDelete.size}.`);
 } catch(error) {
     console.error(error);
 }
