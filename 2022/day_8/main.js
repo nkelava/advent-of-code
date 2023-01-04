23ce0630badf336428c3143f9ae2473a06908ad6
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-04 01:54:29 +0100

    Refactor part 2 of day 8 task

--- 2022/day_8/main.js
+++ 2022/day_8/main.js
@@ -19,7 +19,7 @@ try {
     const highestScenicScore = scenic.findHighestScenicScore(map);
 
     console.log(`Number of trees that are visible from outside the grid is ${visibleTreeCount}.`);
-    console.log(`Highest scenic score is ${highestScenicScore}`);
+    console.log(`Highest scenic score is ${highestScenicScore}.`);
 } catch(error) {
     console.error(error);
 }
--- 2022/day_8/scenicScore.js
+++ 2022/day_8/scenicScore.js
@@ -1,16 +1,13 @@
-function checkEdge(forest, row, column, direction) {
-    if(row === 0 && direction === "up") return true;
-    if(row === forest.length - 1 && direction === "down") return true;
-    if(column === 0 && direction === "left") return true;
-    if(column === forest[0].length - 1 && direction === "right") return true;
+function hasCrossedEdge(forest, currentRow, currentColumn) {
+    if(currentRow < 0) return true; 
+    if(currentColumn < 0) return true;
+    if(currentRow >= forest.length) return true;
+    if(currentColumn >= forest[0].length) return true;
     return false;
 }
 
 function calculate(forest, row, column, direction) {
-    let scenicScore = 0;
-    let currentRow = row;
-    let currentColumn = column;
-    if(checkEdge(forest, row, column, direction)) return 1;
+    let scenicScore = 0, currentRow = row, currentColumn = column;
 
     do {
         switch(direction) {
@@ -28,22 +25,22 @@ function calculate(forest, row, column, direction) {
                 break;
         }
 
-        if(currentRow < 0 || currentColumn < 0 || currentRow >= forest.length || currentColumn >= forest[0].length) break;
-
+        if(hasCrossedEdge(forest, currentRow, currentColumn)) break;
+        
         scenicScore++;
     } while(Number(forest[currentRow][currentColumn]) < Number(forest[row][column]));
 
     return scenicScore;
 }
 
-
 function calculateScenicScore(forest, row, column) {
-    const up = calculate(forest, row, column, "up");
-    const right = calculate(forest, row, column, "right");
-    const down = calculate(forest, row, column, "down");
-    const left = calculate(forest, row, column, "left");
+    const moves = ["up", "right", "down", "left"];
 
-    return up * right * down * left;
+    const product = moves.reduce((product, move) => {
+        return product * calculate(forest, row, column, move);
+    }, 1);
+
+    return product;
 }
 
 function findHighestScenicScore(map) {
