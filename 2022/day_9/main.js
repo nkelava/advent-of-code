93414b1b31357185d3fe4a21c658eca480b4759f
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-07 02:05:27 +0100

    Refactor part 1 of day 9 task

--- 2022/day_9/main.js
+++ 2022/day_9/main.js
@@ -1,13 +1,39 @@
 const fs = require("fs");
-const tail = require("./tail");
+const Knot = require("./point");
+
+function simulateSeriesOfMotions(instructions, knotCount, startX, startY) {
+    const rope = new Array(knotCount).fill(null).map(() => new Knot(startX, startY));
+    let direction, steps, prevHead;
+    let head = rope[0];
+    let tail = rope[1];
+
+    instructions.forEach(instruction => {
+        instruction = instruction.split(" ");
+        direction = instruction[0];
+        steps = Number(instruction[1]);
+        
+        for(let i = 0; i < steps; ++i) {
+            prevHead = new Knot(head.x, head.y);
+            
+            head.move(direction);
+       
+            if(tail.calculateDistance(head) > 1) {
+                tail.changePosition(prevHead.x, prevHead.y);
+            }
+        }
+    });
+
+    return rope;
+}
 
 try {
     const input = fs.readFileSync("input.txt", "utf-8").replace(/\r/g, "").split("\n");
-    const tailVisited = tail.getVisitedPositions(input, 2);
+    let knotCount = 2;
+    const result = simulateSeriesOfMotions(input, knotCount, 0, 0);
+    const tail = result.at(-1);
+    const tailVisitedUnique = tail.getVisitedUnique();
     
-    console.log(tailVisited);
-
-    console.log(`Tail visited ${tailVisited.length} unique position/s.`);
+    console.log(`Tail visited ${tailVisitedUnique.length} unique position/s.`);
 } catch(error) {
     console.log(error);
 }
