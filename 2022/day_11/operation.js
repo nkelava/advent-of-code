030a000f94a28489a00863726fbe856abcc38f7d
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-11 23:13:20 +0100

    Add operation class for day 11 task

---
+++ 2022/day_11/operation.js
@@ -0,0 +1,10 @@
+class Operation {
+    constructor() {
+        this.text = text;
+        this.item = null;
+        this.operator = null;
+        this.value = null;
+    }
+}
+
+module.exports = Operation;
