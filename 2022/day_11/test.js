33db0d1aec5ed4db92c2bfb4f22a2e1502ecc7b4
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-11 23:13:06 +0100

    Add test class

---
+++ 2022/day_11/test.js
@@ -0,0 +1,11 @@
+class Test {
+    constructor(text, ifTrue, ifFalse) {
+        this.text = text;
+        this.operator = null;
+        this.value = null;
+        this.ifTrue = ifTrue;
+        this.ifFalse = ifFalse;
+    }
+}
+
+module.exports = Test;
