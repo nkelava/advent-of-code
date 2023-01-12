336aa11d8e08f8ea9a564ef71cae979e9b94637a
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-12 23:42:04 +0100

    Add keepaway object to main for day 11 task

--- 2022/day_11/main.js
+++ 2022/day_11/main.js
@@ -1,9 +1,13 @@
 const fs = require("fs");
+const KeepAway = require("./keepaway");
 
 try {
-    const input = fs.readFileSync("./input.txt", "utf-8").replace(/\r/g, "").split("\n");
+    const notes = fs.readFileSync("./input.txt", "utf-8").replace(/\r/g, "").split("\n");
+    const keepAway = new KeepAway(notes);
+
+    // keepAway.play();
     
-    console.log(monkeys);
+    console.log(keepAway.monkeys);
 } catch (error) {
     console.error(error);
 }
