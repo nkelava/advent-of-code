b6f8faa918e08115b3f2e30ae4a2bbf51de10b57
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-12 23:42:24 +0100

    Add moneky class property

--- 2022/day_11/monkey.js
+++ 2022/day_11/monkey.js
@@ -2,8 +2,15 @@ class Monkey {
     constructor(id, items, operation, test) {
         this.id = id;
         this.items = [...items];
+        this.itemsInspected = 0;
         this.operation = operation;
         this.test = test;
+    }
+
+    inspect() {
+        this.items.forEach(item => {
+
+        });
     }
 }
 
