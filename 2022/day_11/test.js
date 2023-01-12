a7ad6ee489c5f529994f0e9ea148c266829497b7
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-12 23:42:48 +0100

    Add test attribute parsing for day 11 task

--- 2022/day_11/test.js
+++ 2022/day_11/test.js
@@ -1,10 +1,16 @@
 class Test {
-    constructor(text, ifTrue, ifFalse) {
-        this.text = text;
+    constructor(description, ifTrue, ifFalse) {
+        this.description = description;
         this.operator = null;
         this.value = null;
         this.ifTrue = ifTrue;
         this.ifFalse = ifFalse;
+    }
+
+    parseDescription(description) {
+        // Test: divisible by 13
+        //   If true: throw to monkey 6
+        //   If false: throw to monkey 2
     }
 }
 
