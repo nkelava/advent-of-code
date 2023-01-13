1b9a114241c602a61cae497f8505675e15a4186a
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-13 23:36:43 +0100

    Add properties and parse functios to test class for day 11 task

--- 2022/day_11/test.js
+++ 2022/day_11/test.js
@@ -1,16 +1,38 @@
 class Test {
+    // Test: divisible by 13
+    //   If true: throw to monkey 6
+    //   If false: throw to monkey 2
     constructor(description, ifTrue, ifFalse) {
         this.description = description;
-        this.operator = null;
-        this.value = null;
-        this.ifTrue = ifTrue;
-        this.ifFalse = ifFalse;
+        this.operator = "/";
+        this.value = this.parseValue();
+        this.ifTrueDescription = ifTrue;
+        this.ifFalseDescription = ifFalse;
+        this.ifTrueValue = this.parseIfTrueValue();
+        this.ifFalseValue = this.parseIfFalseValue();
+        this.throwTo = null;
     }
 
-    parseDescription(description) {
-        // Test: divisible by 13
-        //   If true: throw to monkey 6
-        //   If false: throw to monkey 2
+    parseValue() {
+        return Number(this.description.slice(this.description.lastIndexOf(" ") + 1).trim());
+    }
+
+    parseIfTrueValue() {
+        return Number(this.ifTrueDescription.slice(this.ifTrueDescription.lastIndexOf(" ") + 1).trim());
+    }
+
+    parseIfFalseValue() {
+        return Number(this.ifFalseDescription.slice(this.ifFalseDescription.lastIndexOf(" ") + 1).trim());
+    }
+
+    run(item) {
+        switch(this.operator) {
+            case "/":
+                this.throwTo = (item % this.value === 0) ? this.ifTrueValue : this.ifFalseValue;
+                break;
+            default:
+                throw new Error("Something went wrong with test execution.");
+        }
     }
 }
 
