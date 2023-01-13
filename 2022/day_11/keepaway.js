fb056d4ccdf3296fbbcf2d1946083115b52e1796
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-13 23:35:19 +0100

    Rename variables in keepaway class and remove so far unnecessary method

--- 2022/day_11/keepaway.js
+++ 2022/day_11/keepaway.js
@@ -12,31 +12,25 @@ class KeepAway {
         if(monkeyStartIndex >= notes.length) return monkeys;
 
         // Monkey 0:
-        // Starting items: 89, 73, 66, 57, 64, 80
-        // Operation: new = old * 3
-        // Test: divisible by 13
+        //  Starting items: 89, 73, 66, 57, 64, 80
+        //  Operation: new = old * 3
+        //  Test: divisible by 13
         //   If true: throw to monkey 6
         //   If false: throw to monkey 2
     
-        const monkeyAttributes = notes.slice(monkeyStartIndex, notes.indexOf("", monkeyStartIndex));
+        const monkeyAttributes = notes.slice(monkeyStartIndex, monkeyStartIndex + 6);
         const monkeyId = Number(monkeyAttributes[0].slice(7, monkeyAttributes[0].length - 1));
-        const items = monkeyAttributes[1].split(": ")[1].split(",").map(item => Number(item.trimStart()));
+        const items = monkeyAttributes[1].split(": ")[1].split(",").map(item => Number(item.trim()));
         const operation = new Operation(monkeyAttributes[2].split(": ")[1]);
         const testDescription = monkeyAttributes[3].split(": ")[1];
         const ifTrue = monkeyAttributes[4].split(": ")[1];
         const ifFalse = monkeyAttributes[5].split(": ")[1];
         const test = new Test(testDescription, ifTrue, ifFalse);
 
-        const newMonkey = new Monkey(monkeyId, items, operation, test);
-        monkeys.push(newMonkey);
+        const monkey = new Monkey(monkeyId, items, operation, test);
+        monkeys.push(monkey);
     
-        return this.parseNotes(notes, monkeyStartIndex + notes.indexOf("") + 1, monkeys);
-    }
-
-    play() {
-        // for(let i = 0; i < this.rounds; i++) {
-            this.monkeys.forEach(monkey => monkey.inspect());
-        // }
+        return this.parseNotes(notes, monkeyStartIndex + 7, monkeys);
     }
 }
 
