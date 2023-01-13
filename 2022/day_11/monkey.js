b2bcca749422cfc76a464c2a1815c38ca9380163
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-13 23:35:47 +0100

    Remove unnecessary method and property in monkey class for day 11 task

--- 2022/day_11/monkey.js
+++ 2022/day_11/monkey.js
@@ -2,15 +2,8 @@ class Monkey {
     constructor(id, items, operation, test) {
         this.id = id;
         this.items = [...items];
-        this.itemsInspected = 0;
         this.operation = operation;
         this.test = test;
-    }
-
-    inspect() {
-        this.items.forEach(item => {
-
-        });
     }
 }
 
