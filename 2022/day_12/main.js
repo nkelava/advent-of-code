f3dfd4e709646ef42983bc67add33cbdc10c084e
Nikola Kelava <nikola.kelava94@gmail.com>
2023-01-30 23:20:06 +0100

    Add bfs call to main

--- 2022/day_12/main.js
+++ 2022/day_12/main.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
 const AStar = require("./astar");
 const Heightmap = require("./heightmap");
+const BFS = require("./bfs");
 
 try {
     const map = fs.readFileSync("./test.txt", "utf-8").replace(/\r/g, "").split("\n");
     const start = { mark: "S", elevation: "a" }
     const end = { mark: "E", elevation: "z" };
     const heightmap = new Heightmap(map, start, end);
+
     const astar = new AStar();
 
     astar.findPath(heightmap);
-    
     console.log(astar.path.length)
+
+    // const bfs = new BFS();
+    // bfs.findPath(heightmap);
+    // console.log(bfs.path.length);
 } catch (error) {
     console.log(error);
 }
