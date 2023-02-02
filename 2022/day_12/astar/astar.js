0fd8c9765748421570feb6e2265648fe792133a1
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-02 16:09:59 +0100

    Add method for finding shortest path from specified elevation

--- 2022/day_12/astar/astar.js
+++ 2022/day_12/astar/astar.js
@@ -1,8 +1,8 @@
+const { hrtime } = require("process");
 const PriorityQueue = require("./priorityQueue");
 
 class AStar {
     constructor() {
-        this.path = [];
     }
 
     reconstructPath(current) {
@@ -38,14 +38,35 @@ class AStar {
                 if(closedSet.find(neighbour)) continue;
 
                 if(neighbour.isEqual(heightmap.end)) {
-                    this.path = this.reconstructPath(current);
-                    return;
+                    return this.reconstructPath(current);
                 }
 
                 neighbour.calculateScores(heightmap.end);
                 openSet.enqueue(neighbour);
             };
         }
+
+        return [];
+    }
+
+    findMinPathFromElevation(heightmap, startingElevation) {
+        const positions = heightmap.getPositionsByElevation(startingElevation);
+        heightmap.start = positions[0];
+        let path = this.findPath(heightmap);
+        let shortestPath = path;
+
+        for(let i = 1; i < positions.length; i++) {
+            heightmap.start = positions[i];
+            path = this.findPath(heightmap);
+            
+            if(path.length > 0) {
+                if(path.length < shortestPath.length) {
+                    shortestPath = path;
+                }
+            }
+        }
+
+        return shortestPath;
     }
 }
 
