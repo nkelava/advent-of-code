b23b8c984986e63d26cfe145f814ae75b902abcf
Nikola Kelava <nikola.kelava94@gmail.com>
2023-02-05 21:42:51 +0100

    Add test input file for day 13 task

--- 2022/day13/main.js
+++ 2022/day13/main.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 try {
     const packets = fs.readFileSync("./input.txt", "utf-8").replace(/\r/g, "").split("\n");
-
+    
     console.log(packets);
 } catch (error) {
     console.error(error);
