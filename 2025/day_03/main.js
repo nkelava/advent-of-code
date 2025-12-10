64a545098447afb062d8a2446a0d47ea487344c3
Nikola Kelava <nkelava.dev@gmail.com>
2025-12-10 23:24:48 +0100

    feat: improve 2025 day 3 part 2 solution using greedy approach

--- 2025/day_03/main.js
+++ 2025/day_03/main.js
@@ -2,36 +2,25 @@ import fs from "node:fs";
 
 function getLargestJoltage(batteryBank, batteriesToSelect) {
   const bankLength = batteryBank.length;
-  const memo = Array(bankLength + 1)
-    .fill(null)
-    .map(() => Array(batteriesToSelect + 1).fill(null));
+  const result = [];
+  let toRemove = bankLength - batteriesToSelect;
 
-  for (let position = 0; position <= bankLength; position++) {
-    memo[position][0] = "";
-  }
+  for (let i = 0; i < bankLength; i++) {
+    const currentDigit = batteryBank[i];
 
-  for (let position = bankLength - 1; position >= 0; position--) {
-    for (let remaining = 1; remaining <= batteriesToSelect; remaining++) {
-      if (memo[position + 1][remaining] !== null) {
-        memo[position][remaining] = memo[position + 1][remaining];
-      }
-
-      if (memo[position + 1][remaining - 1] !== null) {
-        const withCurrentBattery =
-          batteryBank[position] + memo[position + 1][remaining - 1];
-
-        if (memo[position][remaining] === null) {
-          memo[position][remaining] = withCurrentBattery;
-        } else {
-          if (withCurrentBattery > memo[position][remaining]) {
-            memo[position][remaining] = withCurrentBattery;
-          }
-        }
-      }
+    while (
+      result.length > 0 &&
+      result[result.length - 1] < currentDigit &&
+      toRemove > 0
+    ) {
+      result.pop();
+      toRemove--;
     }
+
+    result.push(currentDigit);
   }
 
-  return memo[0][batteriesToSelect] ? Number(memo[0][batteriesToSelect]) : 0;
+  return Number(result.slice(0, batteriesToSelect).join(""));
 }
 
 function getTotalOutputJoltage(batteryBanks, batteriesToSelect) {
